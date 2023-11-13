ffd9823de434275917c2cab6a7ce7177ef613b40
prestonbrubaker <brubakerpreston@gmail.com>
2023-11-12 19:10:25 -0500

    dassadsad

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+var c = document.getElementById("canvas1");
+var ctx = c.getContext("2d");
 
+// Display settings
+var minW = 0;
+var minH = 0;
+var maxW = canvas1.width;   //width is 800 pixels
+var maxH = canvas1.height;   //height is 800 pixels
+var bgHue = "#777777";
+
+var pixS = 8;
+var pCX = Math.floor(maxW / pixS);
+var pCY = Math.floor(maxH / pixS);
+var pA = new Array(pCY);
+
+var cloneA = new Array(pCY);
+
+
+var tickS = 100;
+
+
+
+function tick() {
+    // Clear and fill background
+    ctx.clearRect(minW, minH, maxW, maxH);
+    ctx.fillStyle = bgHue;
+    ctx.fillRect(minW, minH, maxW, maxH);
+
+}
+
+
+setInterval(tick, tickS);
